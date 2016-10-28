d6256e9f57a2ab14f215d7f6ab5850ae5488e3d9
yangshihe <yangshihe@qq.com>
2016-10-28 12:34:16 +0800

    baidubianjqi

---
+++ UEditor.php
@@ -0,0 +1,54 @@
+<?php
+namespace yangshihe\ueditor;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\helpers\Url;
+use yii\web\View;
+use yii\widgets\InputWidget;
+
+class UEditor extends InputWidget
+{
+    public $clientOptions = [];
+
+    protected $_options;
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function init()
+    {
+        $this->id = $this->hasModel() ? Html::getInputId($this->model, $this->attribute) : $this->id;
+        $this->_options = [
+            'serverUrl' => Url::to(['upload']),
+            'initialFrameWidth' => '100%',
+            'initialFrameHeight' => '400',
+            'lang' => (strtolower(Yii::$app->language) == 'en-us') ? 'en' : 'zh-cn',
+        ];
+        $this->clientOptions = ArrayHelper::merge($this->_options, $this->clientOptions);
+        parent::init();
+    }
+
+    public function run()
+    {
+        $this->registerClientScript();
+        if ($this->hasModel()) {
+            return Html::activeTextarea($this->model, $this->attribute, ['id' => $this->id]);
+        } else {
+            return Html::textarea($this->id, $this->value, ['id' => $this->id]);
+        }
+    }
+
+    /**
+     * 注册客户端脚本
+     */
+    protected function registerClientScript()
+    {
+        UEditorAsset::register($this->view);
+        $clientOptions = Json::encode($this->clientOptions);
+        $script = "UE.getEditor('" . $this->id . "', " . $clientOptions . ")";
+        $this->view->registerJs($script, View::POS_READY);
+    }
+}
