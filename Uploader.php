f9ba462b2b259cd63c3e4eeb1d7dde3f04196f6f
yangshihe <yangshihe@qq.com>
2016-10-28 14:55:13 +0800

    123

--- Uploader.php
+++ Uploader.php
@@ -1,13 +1,7 @@
 <?php
-namespace yangshihe\ueditor;
+namespace kucha\ueditor;
 
-/**
- * Created by JetBrains PhpStorm.
- * User: taoqili
- * Date: 12-7-18
- * Time: 上午11: 32
- * UEditor编辑器通用上传类
- */
+
 class Uploader
 {
     private $fileField; //文件域名
@@ -29,47 +23,45 @@ class Uploader
         "文件未被完整上传",
         "没有文件被上传",
         "上传文件为空",
-        "ERROR_TMP_FILE" => "临时文件错误",
+        "ERROR_TMP_FILE"           => "临时文件错误",
         "ERROR_TMP_FILE_NOT_FOUND" => "找不到临时文件",
-        "ERROR_SIZE_EXCEED" => "文件大小超出网站限制",
-        "ERROR_TYPE_NOT_ALLOWED" => "文件类型不允许",
-        "ERROR_CREATE_DIR" => "目录创建失败",
-        "ERROR_DIR_NOT_WRITEABLE" => "目录没有写权限",
-        "ERROR_FILE_MOVE" => "文件保存时出错",
-        "ERROR_FILE_NOT_FOUND" => "找不到上传文件",
-        "ERROR_WRITE_CONTENT" => "写入文件内容错误",
-        "ERROR_UNKNOWN" => "未知错误",
-        "ERROR_DEAD_LINK" => "链接不可用",
-        "ERROR_HTTP_LINK" => "链接不是http链接",
-        "ERROR_HTTP_CONTENTTYPE" => "链接contentType不正确",
-        "INVALID_URL" => "非法 URL",
-        "INVALID_IP" => "非法 IP"
+        "ERROR_SIZE_EXCEED"        => "文件大小超出网站限制",
+        "ERROR_TYPE_NOT_ALLOWED"   => "文件类型不允许",
+        "ERROR_CREATE_DIR"         => "目录创建失败",
+        "ERROR_DIR_NOT_WRITEABLE"  => "目录没有写权限",
+        "ERROR_FILE_MOVE"          => "文件保存时出错",
+        "ERROR_FILE_NOT_FOUND"     => "找不到上传文件",
+        "ERROR_WRITE_CONTENT"      => "写入文件内容错误",
+        "ERROR_UNKNOWN"            => "未知错误",
+        "ERROR_DEAD_LINK"          => "链接不可用",
+        "ERROR_HTTP_LINK"          => "链接不是http链接",
+        "ERROR_HTTP_CONTENTTYPE"   => "链接contentType不正确"
     );
 
     /**
      * 构造函数
-     * @param string $fileField 表单名称
-     * @param array $config 配置项
-     * @param bool $base64 是否解析base64编码，可省略。若开启，则$fileField代表的是base64编码的字符串表单名
+     * @param $fileField  表单名称
+     * @param $config 配置项
+     * @param string $type 是否解析base64编码，可省略。若开启，则$fileField代表的是base64编码的字符串表单名
      */
     public function __construct($fileField, $config, $type = "upload")
     {
         $this->fileField = $fileField;
         $this->config = $config;
+        $this->type = $type;
+        $this->uploadFilePath = $_SERVER['DOCUMENT_ROOT'];
         if (isset($this->config['uploadFilePath']) && !empty($this->config['uploadFilePath'])) {
 	        $this->uploadFilePath = $this->config['uploadFilePath'];
 	        unset($this->config['uploadFilePath']);
         }
-        $this->type = $type;
         if ($type == "remote") {
             $this->saveRemote();
-        } else if($type == "base64") {
+        } else if ($type == "base64") {
             $this->upBase64();
         } else {
             $this->upFile();
         }
-
-        $this->stateMap['ERROR_TYPE_NOT_ALLOWED'] = iconv('unicode', 'utf-8', $this->stateMap['ERROR_TYPE_NOT_ALLOWED']);
+//        $this->stateMap['ERROR_TYPE_NOT_ALLOWED'] = iconv('unicode', 'utf-8', $this->stateMap['ERROR_TYPE_NOT_ALLOWED']);
     }
 
     /**
@@ -122,7 +114,6 @@ class Uploader
             $this->stateInfo = $this->getStateInfo("ERROR_DIR_NOT_WRITEABLE");
             return;
         }
-
         //移动文件
         if (!(move_uploaded_file($file["tmp_name"], $this->filePath) && file_exists($this->filePath))) { //移动失败
             $this->stateInfo = $this->getStateInfo("ERROR_FILE_MOVE");
@@ -186,36 +177,15 @@ class Uploader
             $this->stateInfo = $this->getStateInfo("ERROR_HTTP_LINK");
             return;
         }
-
-        preg_match('/(^https*:\/\/[^:\/]+)/', $imgUrl, $matches);
-        $host_with_protocol = count($matches) > 1 ? $matches[1] : '';
-
-        // 判断是否是合法 url
-        if (!filter_var($host_with_protocol, FILTER_VALIDATE_URL)) {
-            $this->stateInfo = $this->getStateInfo("INVALID_URL");
-            return;
-        }
-
-        preg_match('/^https*:\/\/(.+)/', $host_with_protocol, $matches);
-        $host_without_protocol = count($matches) > 1 ? $matches[1] : '';
-
-        // 此时提取出来的可能是 ip 也有可能是域名，先获取 ip
-        $ip = gethostbyname($host_without_protocol);
-        // 判断是否是私有 ip
-        if(!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE)) {
-            $this->stateInfo = $this->getStateInfo("INVALID_IP");
-            return;
-        }
-
         //获取请求头并检测死链
-        $heads = get_headers($imgUrl, 1);
+        $heads = get_headers($imgUrl);
         if (!(stristr($heads[0], "200") && stristr($heads[0], "OK"))) {
             $this->stateInfo = $this->getStateInfo("ERROR_DEAD_LINK");
             return;
         }
         //格式验证(扩展名验证和Content-Type验证)
         $fileType = strtolower(strrchr($imgUrl, '.'));
-        if (!in_array($fileType, $this->config['allowFiles']) || !isset($heads['Content-Type']) || !stristr($heads['Content-Type'], "image")) {
+        if (!in_array($fileType, $this->config['allowFiles']) || stristr($heads['Content-Type'], "image")) {
             $this->stateInfo = $this->getStateInfo("ERROR_HTTP_CONTENTTYPE");
             return;
         }
@@ -232,7 +202,7 @@ class Uploader
         ob_end_clean();
         preg_match("/[\/]([^\/]*)[\.]?[^\.\/]*$/", $imgUrl, $m);
 
-        $this->oriName = $m ? $m[1]:"";
+        $this->oriName = $m ? $m[1] : "";
         $this->fileSize = strlen($img);
         $this->fileType = $this->getFileExt();
         $this->fullName = $this->getFullName();
@@ -308,7 +278,7 @@ class Uploader
         $format = str_replace("{filename}", $oriName, $format);
 
         //替换随机字符串
-        $randNum = rand(1, 10000000000) . rand(1, 10000000000);
+        $randNum = mt_rand(1, 1000000000) . mt_rand(1, 1000000000);
         if (preg_match("/\{rand\:([\d]*)\}/i", $format, $matches)) {
             $format = preg_replace("/\{rand\:[\d]*\}/i", substr($randNum, 0, $matches[1]), $format);
         }
@@ -321,7 +291,8 @@ class Uploader
      * 获取文件名
      * @return string
      */
-    private function getFileName () {
+    private function getFileName()
+    {
         return substr($this->filePath, strrpos($this->filePath, '/') + 1);
     }
 
@@ -332,13 +303,12 @@ class Uploader
     private function getFilePath()
     {
         $fullname = $this->fullName;
-        $rootPath = $_SERVER['DOCUMENT_ROOT'];
-        if (!empty($this->uploadFilePath))
-        	$rootPath = $this->uploadFilePath;
+
         if (substr($fullname, 0, 1) != '/') {
             $fullname = '/' . $fullname;
         }
-        return $rootPath . $fullname;
+
+        return $this->uploadFilePath . $fullname;
     }
 
     /**
@@ -366,13 +336,12 @@ class Uploader
     public function getFileInfo()
     {
         return array(
-            "state" => $this->stateInfo,
-            "url" => $this->fullName,
-            "title" => $this->fileName,
+            "state"    => $this->stateInfo,
+            "url"      => $this->fullName,
+            "title"    => $this->fileName,
             "original" => $this->oriName,
-            "type" => $this->fileType,
-            "size" => $this->fileSize
+            "type"     => $this->fileType,
+            "size"     => $this->fileSize
         );
     }
-
 }
